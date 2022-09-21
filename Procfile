8a3c793882680e7619c76b522fb95cc24116b4de
Mahmuda Karima <dakasakti.id@gmail.com>
2022-09-21 21:40:15 +0700

    deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: deploy-apps-hexagonal
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/dakasakti/deploy-apps-hexagonal/config"
+	"github.com/dakasakti/deploy-apps-hexagonal/internal/factory"
+	"github.com/dakasakti/deploy-apps-hexagonal/internal/http"
+	"github.com/dakasakti/deploy-apps-hexagonal/internal/middlewares"
+	"github.com/dakasakti/deploy-apps-hexagonal/internal/routes"
+)
+
+func main() {
+	c := config.GetConfig()
+	f := factory.NewFactory(c)
+
+	e := routes.New()
+	middlewares.LoggerMiddleware(e)
+
+	http.NewHttp(e, f)
+	e.Logger.Fatal(e.Start(":8000"))
+}
